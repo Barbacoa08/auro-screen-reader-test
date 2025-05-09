550a3d20cc8d35173b5f97c36485b78e62c4170b
barbajoe <sabe12070@gmail.com>
2025-05-09 18:27:38 -0500

    add dialog example

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 
 import "./App.css";
 import "@aurodesignsystem/auro-alert";
-import "@aurodesignsystem/auro-icon";
 import "@aurodesignsystem/auro-button";
+import "@aurodesignsystem/auro-dialog";
 import "@aurodesignsystem/auro-formkit/auro-dropdown";
+import "@aurodesignsystem/auro-icon";
 import "@aurodesignsystem/auro-popover";
 import "@aurodesignsystem/design-tokens/dist/alaska/CSSCustomProperties--alaska.css";
 
 export const App = () => {
 	const [alertVisible, setAlertVisible] = useState(false);
+	const [dialogOpen, setDialogOpen] = useState(false);
 
 	return (
 		<main>
@@ -58,6 +60,58 @@ export const App = () => {
 						<a href="/">thing three</a>
 					</div>
 				</auro-dropdown>
+			</section>
+
+			<section>
+				<h2>Auro Dialog test</h2>
+
+				<div>
+					<auro-button onClick={() => setDialogOpen(true)}>
+						Open default dialog
+					</auro-button>
+				</div>
+
+				<auro-dialog open={dialogOpen} modal>
+					<span slot="header">Default Dialog</span>
+
+					<div slot="content">
+						<p>
+							When traveling on Alaska Airlines flights, Alaska Airlines checked
+							baggage fees may apply. See{" "}
+							<auro-hyperlink
+								href="https://www.alaskaair.com/bagrules"
+								target="_blank"
+							>
+								alaskaair.com/bagrules
+							</auro-hyperlink>{" "}
+							for our rules. For itineraries that include other airlines, their
+							checked baggage fees may apply, as displayed on their websites.
+						</p>
+						<p>
+							Baggage rules and fees will be based on the specific itinerary
+							chosen. The applicable first and second bag fees will be displayed
+							after you have added flights to the cart.
+						</p>
+						<auro-header level="3" display="500">
+							Before checking your bags, remember to:
+						</auro-header>
+						<ul>
+							<li>Caerphilly croque monsieur fondue</li>
+							<li>
+								Taleggio goat mascarpone cow manchego cheese and wine emmental
+								cheese strings
+							</li>
+							<li>Cheddar cheese and biscuits chalk and cheese</li>
+							<li>Camembert de normandie stinking bishop bavarian bergkase</li>
+						</ul>
+					</div>
+
+					<div slot="footer">
+						<auro-button secondary onClick={() => setDialogOpen(false)}>
+							Close
+						</auro-button>
+					</div>
+				</auro-dialog>
 			</section>
 		</main>
 	);
